b92e56660e9aed1e434b9875f0c276a0e91a3e17
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-08-04 09:48:07 +0430

    fix scissors animation bug

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,16 +1,8 @@
-const button = document.querySelector('#play__button');
-  button.addEventListener('click',(e)=>{
-    e.preventDefault();
-    button.classList.add('btn--clicked');
-    document.querySelectorAll('span').forEach((element)=>{element.classList.add('expanded')})
-    
-    
-    
-    
-    /* 
----------------------------------------
-just to reset without having to refresh..
- --------------------------------*/
-    //  setTimeout(()=>{button.classList.remove("btn--clicked")},3500);
-    //  setTimeout(()=>{document.querySelectorAll('span').forEach((element)=>{element.classList.remove('expanded')})},1700)
-    });
+'use strict';
+let $ = document;
+let movingScissors = $.querySelector('#moving_scissors');
+// remove scissors when animation ends
+movingScissors.addEventListener('animationend', function () {
+  this.hidden = true;
+});
+//
