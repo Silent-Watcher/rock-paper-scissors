84caa9cb56ce1e4e6c9567574b2189ac916586fe
Ali.t.nazari <91375198+Silent-Watcher@users.noreply.github.com>
2022-08-06 22:45:14 +0430

    add copyright

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -51,7 +51,7 @@ const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
   showConfirmButton: false,
-  timer: 3500,
+  timer: 3000,
   timerProgressBar: true,
   didOpen: (toast) => {
     toast.addEventListener('mouseenter', Swal.stopTimer);
